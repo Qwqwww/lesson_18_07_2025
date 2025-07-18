2e29bd0ce8e6bfe44e35818a45702fd3c9374ded
Qwqwww <jollot@yandex.ru>
2025-07-19 00:36:26 +0700

    Добавлена форма

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
 
-@app.route('/')  # 127.0.0.1:5000/
+@app.route('/', methods=["get", "post"])  # 127.0.0.1:5000/
 def index():
+    if request.method == "POST":
+        area = request.form.get("area")
+        print(area)
     return render_template("index.html")
 
 
