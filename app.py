955fe97bf9b422c1df6b803cf1aa3cef1b1f5389
Qwqwww <jollot@yandex.ru>
2025-07-19 00:29:25 +0700

    Добавлен файл HTML

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')  # 127.0.0.1:5000/
 def index():
-    return "<h1>Hello!</h1><p>Text</p>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
