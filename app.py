3c674bb4d314e4300355202635eb6efb6a68c5ba
Qwqwww <jollot@yandex.ru>
2025-07-19 00:46:08 +0700

    TODO для доработок

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index():
     text = ""
     if request.method == "POST":
         area = request.form.get("area")
+        # TODO: нормализация, стандартизация, ML-модели... 
         text = f"Стоимость квартиры площадью {area} кв. м. равна {float(area) * 100_000} рублей"
     return render_template("index.html", message=text)
 
